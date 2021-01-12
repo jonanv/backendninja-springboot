922170b6ed78072057fb2d1246985100994350e0
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-12 18:31:07 -0500

    Se crea una nueva vista que recibe la peticion POST y mostrada como objeto Person

--- src/main/java/com/udemy/cotroller/Example3Controller.java
+++ src/main/java/com/udemy/cotroller/Example3Controller.java
@@ -5,13 +5,17 @@ import com.udemy.model.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/example3")
 public class Example3Controller {
 
     private static final String FORM_VIEW = "form";
+    private static final String RESULT_VIEW = "addperson";
 
     @GetMapping("/form")
     public String showForm(Model model) {
@@ -19,4 +23,10 @@ public class Example3Controller {
         return FORM_VIEW;
     }
     
+    @PostMapping("/addperson")
+    public ModelAndView addPerson(@ModelAttribute("person") Person person) {
+        ModelAndView mav = new ModelAndView(RESULT_VIEW);
+        mav.addObject("person", person);
+        return mav;
+    }
 }
