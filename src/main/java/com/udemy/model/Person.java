595f1964b79e08b91a3ccd21df89ad202c87dc18
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-11 19:35:40 -0500

    Se generan los getter y setter para el modelo Person y su contructor con los dos atributos

--- src/main/java/com/udemy/model/Person.java
+++ src/main/java/com/udemy/model/Person.java
@@ -1,6 +1,27 @@
 package com.udemy.model;
 
 public class Person {
-    private int name;
+    private String name;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
 }
