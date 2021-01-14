8544cc93bce7d6bf868de39d5aedf12ed11fb82a
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-13 20:54:14 -0500

    Se creo controlador para controlar los errores y se renderiza la vista de Internal Server Error, se translada la vista a la carpera error y se modifica la imagen con thymeleaf en el vista 500.html

--- src/main/java/com/udemy/cotroller/Example3Controller.java
+++ src/main/java/com/udemy/cotroller/Example3Controller.java
@@ -33,6 +33,7 @@ public class Example3Controller {
     @GetMapping("/showform")
     public String showForm(Model model) {
         model.addAttribute("person", new Person());
+        int i = 6 / 0;
         return FORM_VIEW;
     }
     
