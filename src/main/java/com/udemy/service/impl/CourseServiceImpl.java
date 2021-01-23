553753ddbd504b20dbc37c08e568eb52c744fc44
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-22 20:24:14 -0500

    Se creo servicio para Course y se creo la implementacion del servicio

---
+++ src/main/java/com/udemy/service/impl/CourseServiceImpl.java
@@ -0,0 +1,30 @@
+package com.udemy.service.impl;
+
+import java.util.List;
+
+import com.udemy.entity.Course;
+import com.udemy.service.CourseService;
+
+public class CourseServiceImpl implements CourseService {
+
+    @Override
+    public Course addCourse(Course course) {
+        return null;
+    }
+
+    @Override
+    public List<Course> listAllCourses() {
+        return null;
+    }
+
+    @Override
+    public int removeCourse(int id) {
+        return 0;
+    }
+
+    @Override
+    public Course updateCourse(Course course) {
+        return null;
+    }
+    
+}
