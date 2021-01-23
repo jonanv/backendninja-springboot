7281ad6bf64425cd9040e8b0f37eba264da7933b
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-22 20:27:30 -0500

    Se inyecta dependencia de CourseJpaRepository en la implementacion del servicio CourseService

--- src/main/java/com/udemy/service/impl/CourseServiceImpl.java
+++ src/main/java/com/udemy/service/impl/CourseServiceImpl.java
@@ -3,9 +3,17 @@ package com.udemy.service.impl;
 import java.util.List;
 
 import com.udemy.entity.Course;
+import com.udemy.repository.CourseJpaRepository;
 import com.udemy.service.CourseService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class CourseServiceImpl implements CourseService {
+
+    @Autowired
+    @Qualifier("courseJpaRepository")
+    private CourseJpaRepository courseJpaRepository;
 
     @Override
     public Course addCourse(Course course) {
