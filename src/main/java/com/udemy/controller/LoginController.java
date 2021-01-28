a8e8175e1082bc09b1df8b23de7f8a93352cee17
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-27 20:25:11 -0500

    Se crea controlador LoginController.java con tres metodos, uno que redirecciona, otro va al login y otro va a contacs que recibe el parametro UserCredencials y error, se organiza la template y se crea model de UserCredencial.java

--- src/main/java/com/udemy/controller/LoginController.java
+++ src/main/java/com/udemy/controller/LoginController.java
@@ -1,11 +1,18 @@
 package com.udemy.controller;
 
+import com.udemy.model.UserCredential;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+
+    private static final String LOGIN_VIEW = "login";
     
     @GetMapping("/")
     public String redirectToLogin() {
@@ -13,13 +20,19 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String showLoginForm() {
-        return "login";
+    public String showLoginForm(Model model, 
+            @RequestParam(name="error", required=false) String error) {
+        model.addAttribute("error", error);
+        model.addAttribute("userCredential", new UserCredential());
+        return LOGIN_VIEW;
     }
 
-    @PostMapping("/")
-    public String loginCheck() {
-        return "contacts";
+    @PostMapping("/logincheck")
+    public String loginCheck(@ModelAttribute(name="userCredential") UserCredential userCredential) {
+        if (userCredential.getUsername().equals("user") && userCredential.getPassword().equals("user")) {
+            return "contacts";
+        }
+        return "redirect:/login?error";
     }
 
 }
