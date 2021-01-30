d0a2758bc7a37f4fc1e077393e9583910978a08b
Giovanni Vargas <jonan-vargas23@hotmail.com>
2021-01-29 20:33:15 -0500

    Se agrega metodo cancel() al controller ContactController.java para la redireccion de la vista contactform y la vista contact

--- src/main/java/com/udemy/controller/ContactController.java
+++ src/main/java/com/udemy/controller/ContactController.java
@@ -1,10 +1,12 @@
 package com.udemy.controller;
 
 import com.udemy.constant.ViewConstant;
+import com.udemy.model.ContactModel;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,10 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class ContactController {
 
     private static final Log LOG = LogFactory.getLog(ContactController.class);
+
+    @GetMapping("/cancel")
+    public String cancel() {
+        return ViewConstant.CONTACTS_VIEW;
+    }
     
     @GetMapping("/contactform")
-    public String redirectContactForm() {
+    public String redirectContactForm(Model model) {
         LOG.info("METHOD: redirectContactForm()");
+        model.addAttribute("contactModel", new ContactModel());
         return ViewConstant.CONTACTFORM_VIEW;
     }
 }
